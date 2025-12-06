a8d12ac25bd6c4651127bacc72fe30befaa5deda
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2025-12-07 02:49:37 +0530

    admin middleware added

---
+++ server/src/middlewares/admin.middlewares.js
@@ -0,0 +1,8 @@
+export const isAdmin=(req,res,next)=>{
+    if(!req.user || req.user.role!=='admin'){
+        return res.status(403).json({
+            success:false,
+            message:"Unauthorized ACCESS"})
+    }
+    next();
+}
