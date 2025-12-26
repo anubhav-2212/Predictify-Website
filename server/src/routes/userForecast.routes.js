38561c9cd57d64679a98f46bf5c36cd7bf810cf3
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2025-12-26 23:13:51 +0530

    getForecastHistory router added

--- server/src/routes/userForecast.routes.js
+++ server/src/routes/userForecast.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import userForecastController, { submitForecast } from "../controller/userForecast.controller.js";
+import userForecastController, { getForecastHistory, submitForecast } from "../controller/userForecast.controller.js";
 import userMiddleware from "../middlewares/auth.middlewares.js";
 const userForecastRouter=express.Router();
 
 userForecastRouter.post('/submit/:forecastId',userMiddleware,submitForecast)
+userForecastRouter.get('/forecast-history',userMiddleware,getForecastHistory)
 
-export default userForecastRouter
+export default userForecastRouter 
